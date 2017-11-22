b3a1ea163b3cfb7a3a3e684e253fd459a7a72184
Duong Thanh Nguyen <nduongthanh@yahoo.com>
2017-11-22 16:36:56 +0100

    Implement step 2 and step 3

--- hello-git.py
+++ hello-git.py
@@ -1,4 +1,18 @@
 print("Hello git!")
 
-def sum():
-    pass
+def sum(n):
+    sum = 0
+    for i in xrange(n):
+        if i%5 or i%3:
+            sum += i
+
+def fib(n):
+    if n<2:
+        return n
+    return fib(n-1) + fib(n-2)
+
+print (sum(1000))
+print (fib(0))
+print (fib(1))
+print (fib(10))
+print (fib(30))
